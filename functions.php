cd0f2c398d2fc0dc1ba8ebb9d2206fbaff8d9b8a
Ibrahim Hossen <48528670+ismdibrahim@users.noreply.github.com>
2024-07-27 12:59:07 +0600

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,108 @@
+/**
+ * WooCommerce User Registration Shortcode
+ */
+
+add_shortcode( 'ed_registration_form', 'ed_separate_register_form' );
+function ed_separate_register_form() {
+   if ( is_admin() ) return;
+   if ( is_user_logged_in() ) return '<p>You are already logged in</p>';
+   ob_start();
+ 
+   // NOTE: THE FOLLOWING <FORM></FORM> IS COPIED FROM woocommerce\templates\myaccount\form-login.php
+   // IF WOOCOMMERCE RELEASES AN UPDATE TO THAT TEMPLATE, YOU MUST CHANGE THIS ACCORDINGLY
+ 
+   do_action( 'woocommerce_before_customer_login_form' );
+ 
+   ?>
+      <form method="post" class="woocommerce-form woocommerce-form-register register" <?php do_action( 'woocommerce_register_form_tag' ); ?> >
+ 
+         <?php do_action( 'woocommerce_register_form_start' ); ?>
+ 
+         <?php if ( 'no' === get_option( 'woocommerce_registration_generate_username' ) ) : ?>
+ 
+            <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+               <label for="reg_username"><?php esc_html_e( 'Username', 'woocommerce' ); ?> <span class="required">*</span></label>
+               <input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="username" id="reg_username" autocomplete="username" value="<?php echo ( ! empty( $_POST['username'] ) ) ? esc_attr( wp_unslash( $_POST['username'] ) ) : ''; ?>" /><?php // @codingStandardsIgnoreLine ?>
+            </p>
+ 
+         <?php endif; ?>
+ 
+         <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+            <label for="reg_email"><?php esc_html_e( 'Email address', 'woocommerce' ); ?> <span class="required">*</span></label>
+            <input type="email" class="woocommerce-Input woocommerce-Input--text input-text" name="email" id="reg_email" autocomplete="email" value="<?php echo ( ! empty( $_POST['email'] ) ) ? esc_attr( wp_unslash( $_POST['email'] ) ) : ''; ?>" /><?php // @codingStandardsIgnoreLine ?>
+         </p>
+ 
+         <?php if ( 'no' === get_option( 'woocommerce_registration_generate_password' ) ) : ?>
+ 
+            <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+               <label for="reg_password"><?php esc_html_e( 'Password', 'woocommerce' ); ?> <span class="required">*</span></label>
+               <input type="password" class="woocommerce-Input woocommerce-Input--text input-text" name="password" id="reg_password" autocomplete="new-password" />
+            </p>
+ 
+         <?php else : ?>
+ 
+            <p><?php esc_html_e( 'A password will be sent to your email address.', 'woocommerce' ); ?></p>
+ 
+         <?php endif; ?>
+ 
+         <?php do_action( 'woocommerce_register_form' ); ?>
+ 
+         <p class="woocommerce-FormRow form-row">
+            <?php wp_nonce_field( 'woocommerce-register', 'woocommerce-register-nonce' ); ?>
+            <button type="submit" class="woocommerce-Button woocommerce-button button woocommerce-form-register__submit" name="register" value="<?php esc_attr_e( 'Register', 'woocommerce' ); ?>"><?php esc_html_e( 'Register', 'woocommerce' ); ?></button>
+         </p>
+ 
+         <?php do_action( 'woocommerce_register_form_end' ); ?>
+ 
+      </form>
+ 
+   <?php
+     
+   return ob_get_clean();
+}
+
+/**
+ * WooCommerce User Login Shortcode
+ */
+  
+add_shortcode( 'ed_login_form', 'ed_separate_login_form' );
+function ed_separate_login_form() {
+   if ( is_user_logged_in() ) return '<p>You are already logged in</p>'; 
+   ob_start();
+   do_action( 'woocommerce_before_customer_login_form' );
+   woocommerce_login_form( array( 'redirect' => wc_get_page_permalink( 'myaccount' ) ) );
+   return ob_get_clean();
+}
+
+
+/**
+ * Redirect Login/Registration to My Account if user login.
+ */
+ 
+add_action( 'template_redirect', 'ed_redirect_login_registration_if_logged_in' );
+function ed_redirect_login_registration_if_logged_in() {
+    if ( is_page() && is_user_logged_in() && ( has_shortcode( get_the_content(), 'ed_separate_login_form' ) || has_shortcode( get_the_content(), 'ed_registration_form' ) ) ) {
+        wp_safe_redirect( wc_get_page_permalink( 'myaccount' ) );
+        exit;
+    }
+}
+
+/**
+ * Custom Redirect for Registrations @ WooCommerce My Account
+ */
+ 
+add_filter( 'woocommerce_registration_redirect', 'ed_customer_register_redirect' );
+function ed_customer_register_redirect( $redirect_url ) {
+   $redirect_url = '/mijn-account/';  // type your my account page url
+   return $redirect_url;
+}
+
+/**
+ * Custom Redirect for Logouts @ WooCommerce My Account
+ */
+ 
+add_action( 'wp_logout', 'owp_redirect_after_logout' );
+function owp_redirect_after_logout() {
+         wp_redirect( 'https://bukiboek.nl/login/' ); // type your logout page url
+         exit();
+}
